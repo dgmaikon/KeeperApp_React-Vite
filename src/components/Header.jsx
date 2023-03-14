d0c6872a985fbf51789712fdce33d78074a20d72
dgmaikon <dgmaikon@hotmail.com>
2023-03-13 21:04:10 -0300

    Remove css import from file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import '../assets/css/style.css'
 
-const Header = () => {
-    return (
-        <header>
-            <h1>Keeper</h1>
-        </header>
-    );
+function Header() {
+  return (
+    <header>
+      <h1>Keeper</h1>
+    </header>
+  );
 }
 
 export default Header;
