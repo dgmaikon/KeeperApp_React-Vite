8c7542ff1915cb8deccd12ba5c3d2413378f186b
dgmaikon <dgmaikon@hotmail.com>
2023-03-13 21:07:08 -0300

    Refactor the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-const currentYear = new Date();
-let year = currentYear.getFullYear();
-
-const Footer = () => {
-    return(
-        <footer>
-            <p>Copyright { year }</p>
-        </footer>
-    );
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
